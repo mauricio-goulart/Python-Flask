adc58e46a40ac679b033adecededd2463e32c12a
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2025-01-15 12:34:53 -0300

    Server Local

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,27 @@
 from app import app
-from flask import render_template
+from flask import flash,redirect,request,render_template
 
-@app.route("/")
-@app.route("/base")
-def base():
-    return render_template("base.html")
+@app.route("/", defaults = {"nome":"drake"})
+@app.route("/base", defaults = {"nome":"drake"})
+@app.route("/base/<nome>")
+def base(nome):
+    return render_template("base.html", nome = nome)
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/autenticar", methods=["POST"])
+def autenticar():
+    usuario = request.form.get("usuario")
+    senha = request.form.get("senha")
+    if (usuario == "admin") and (senha == "debug"):
+        return f"usuario: {usuario} e senha: {senha}"
+    else:
+        flash("Errou, o usuario é admin e a senha é debug" )
+        return redirect("/login")
+
+    
 
 @app.route("/index")
 def index(): 
@@ -13,4 +30,4 @@ def index():
 @app.route("/contato")
 def contato():
     return render_template("contato.html")
-
+ 
--- flaskenv
+++ flaskenv
@@ -1,1 +1,2 @@
 FLASK_APP=volk.py
+FLASK_ENV=development
